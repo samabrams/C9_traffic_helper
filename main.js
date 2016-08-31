a808c5b2ef682cb9eb6d21fe260d624c96d7212d
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-31 15:54:59 -0700

    added markers

--- main.js
+++ main.js
@@ -76,12 +76,22 @@ function initMap() {
     });
 
     var lfz = {lat: 33.636193, lng: -117.739393};
+    // var marker = new google.maps.Marker({
+    //     position: lfz,
+    //     map: map,
+    //     title: 'Learning Fuze',
+    //     icon: 'images/mark.png'
+    // });
+
+
+
+    // Markers - testing if I can add clickable markers as inputs
     var marker = new google.maps.Marker({
         position: lfz,
         map: map,
-        title: 'Learning Fuze'
+        title: 'Set Origin',
+        icon: 'images/mark.png'
     });
-
     var infoWindow = new google.maps.InfoWindow({
         content: 'Dan is the man!'
 
@@ -90,32 +100,33 @@ function initMap() {
         infoWindow.open(map, marker);
     });
 
-
-    // Markers - testing if I can add clickable markers as inputs
-    var marker = new google.maps.Marker({
-        position: lfz,
-        map: map,
-        title: 'Set Origin'
-    });
-
-    map.addListener('center_changed', function() {
-        // 3 seconds after the center of the map has changed, pan back to the
-        // marker.
-        window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-        }, 3000);
-    });
-
     marker.addListener('click', function() {
         map.setZoom(15);
         map.setCenter(marker.getPosition());
         console.log('after clicking marker, the marker.getPosition is : ', marker.getPosition());
     });
 }
+
 // // Adds the traffic layer
 //     var trafficLayer = new google.maps.TrafficLayer();
 //     console.log(trafficLayer);
 //     trafficLayer.setMap(map);
+//
+// Origins and Destinations
+//
+// var onRamps = [
+//     {
+//         position: new google.maps.LatLng(33.636193, -117.739393),
+//         type: 'info'
+//     }
+// ];
+//
+// var offRamps = [
+//     {
+//         position: new google.maps.LatLng(33.636193, -117.739393),
+//         type: 'info'
+//     }
+// ];
 
 
 // Defines map style - set in the initMap
