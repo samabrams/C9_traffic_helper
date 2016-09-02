1640e172244bd55a7fc1d78810f140554ba7089c
Brian Phan <brianphan88@gmail.com>
2016-09-02 12:01:32 -0700

    last save

--- php_sql_commands/getCalc.php
+++ php_sql_commands/getCalc.php
@@ -1,16 +1,37 @@
 <?php
-function getCalc($startKey, $endKey, $day){
-    global $output, $link;
-    $output['data']['calc'] = [];
+function getCalc(){
+    $link = mysqli_connect('iamleonhardt.com', 'Brian', 'ireallyliketraffic', 'traffichelper');
+    //global $output, $link;
+    $output['data']['calc'] = ['test'];
 
-    $speedQuery = 'SELECT * FROM `i5_speed_5_stations` WHERE';
+    //$startId = $_POST['startKey'];  //int coressponding to ID
+    //$endId = $_POST['endKey'];      //int coressponding to ID
+    //$date = $_POST['date'];         //day of week as number 1=Sun - 7=Sat
+    $startId = 1204198;
+    $endId =1204230;
+    $date =2;
 
-    $results = mysqli_query($link, $speedQuery);
-    $rows = mysqli_num_rows($results);
+    $startKeyId = 'SELECT primaryKey FROM i5_stations WHERE ID=' . $startId;    //returns 1
+    $endKeyId = 'SELECT primaryKey FROM i5_stations WHERE ID=' . $endId;        //returns 3
 
-    for ($i = 0; $i < $rows; $i++) {
-        $data = mysqli_fetch_assoc($results);
-        array_push($output['data']['onRamp'], $data);
-    }
+    $stationIDQuery = 'SELECT ID FROM `i5_stations` WHERE primaryKey BETWEEN ' . $startKeyId . ' AND ' . $endKeyId; //returns ID of 123
+
+    $timeDuration = 'SELECT * FROM i5_speed_5_stations WHERE DAYOFWEEK(time_date) = ' . $date; //returns all rows corresponding to the day of week
+
+
+//    $results = mysqli_query($link, $speedQuery);
+//    $rows = mysqli_num_rows($results);
+//
+//    for ($i = 0; $i < $rows; $i++) {
+//        $data = mysqli_fetch_assoc($results);
+//        array_push($output['data']['onRamp'], $data);
+//    }
+
+    //$callStartKeyID = mysqli_query($link, $startKeyId);
+    //$callEndKeyID = mysqli_query($link, $endKeyId);
+
+
+
 }
+
 ?>
