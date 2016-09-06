28fe8d6b097d682c172b1496f2cf4541ff08bc6b
Brian Phan <brianphan88@gmail.com>
2016-09-06 11:13:12 -0700

    last save

---
+++ traffic_db_connect.php
@@ -0,0 +1,5 @@
+<?php
+
+    $link = mysqli_connect('iamleonhardt.com', 'Brian', 'uVyFVjupbBxxeVHh', 'traffichelper');
+//    $link = mysqli_connect('localhost', 'root', 'root', 'c9_traffic_helper', 8889);
+?>
